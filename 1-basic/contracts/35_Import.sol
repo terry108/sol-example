70924c819a932ded16baed0498c3ccf2a6a12550
terry108 <996240986@qq.com>
2021-09-08 16:00:04 +0800

    update

---
+++ 1-basic/contracts/35_Import.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+// https://github.com/owner/repo/blob/branch/path/to/Contract.sol
+import "https://github.com/owner/repo/blob/branch/path/to/Contract.sol";
+
+// Example import ECDSA.sol from openzeppelin-contract repo, release-v3.3 branch
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.3/contracts/cryptography/ECDSA.sol
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.3/contracts/cryptography/ECDSA.sol";
+
+
+contract Foo {
+    string public name = "Foo";
+}
